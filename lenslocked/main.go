a83a8a4a7630579506652903b330342d075f4d17
bhagyashree-tiwari <reachbhagyashree.tiwari@gmail.com>
2023-10-04 17:36:33 +0530

    added contact page handler

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -5,17 +5,22 @@ import (
 	"net/http"
 )
 
-// handlerfunc is a function that handles incoming HTTP requests.
-func handlerfunc(w http.ResponseWriter, r *http.Request) {
+// homeHandler is a function that handles incoming HTTP requests.
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	// Respond to the request with an HTML message.
 	fmt.Fprint(w, "<h1>Welcome to my site!</h1>")
 }
 
-func main() {
-	// Register the handlerfunc function to handle requests at the root ("/") path.
-	http.HandleFunc("/", handlerfunc)
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:bhagyashreetiwari12@gmail.com\">bhagyashreetiwari12@gmail.com</a>. </p>")
 
+}
+func main() {
+	// Register the homeHandler function to handle requests at the root ("/") path.
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000....")
 
 	// Start the HTTP server on port 3000 and handle incoming requests using the default router (nil).
