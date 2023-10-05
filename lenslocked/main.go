b6766281c911e845e7ae048056e9831d2fe049f3
bhagyashree-tiwari <reachbhagyashree.tiwari@gmail.com>
2023-10-05 17:55:24 +0530

    working with handlers

--- lenslocked/main.go
+++ lenslocked/main.go
@@ -14,13 +14,31 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:bhagyashreetiwari12@gmail.com\">bhagyashreetiwari12@gmail.com</a>. </p>")
+	fmt.Fprint(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:bhagyashreetiwari12@gmail.com\">bhagyashreetiwari12@gmail.com</a> </p>")
 
 }
+
+func pathHandler(w http.ResponseWriter, r *http.Request) {
+
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		// Handle page not found error
+		http.Error(w, "Page Not Found", http.StatusNotFound)
+		// w.WriteHeader(http.StatusNotFound)
+		// fmt.Fprint(w, "Page Not Found")
+	}
+
+}
+
 func main() {
 	// Register the homeHandler function to handle requests at the root ("/") path.
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	http.HandleFunc("/", pathHandler)
+
+	// http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000....")
 
 	// Start the HTTP server on port 3000 and handle incoming requests using the default router (nil).
